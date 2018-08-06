0a75aa4893d7eabfb4a5ac68dcc67bd96ee40377
Renan GEHAN <rgehan94@gmail.com>
2018-08-06 21:57:11 +0200

    Lock rendering behind PersistGate

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { getPersistor } from '@rematch/persist';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 
 import { App } from './App';
 import { store } from './redux';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <PersistGate persistor={getPersistor()}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </PersistGate>,
   document.getElementById('container')
 );
