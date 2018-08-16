c9819a3deda55577d52b88aa076e502ae89e702d
Renan GEHAN <rgehan94@gmail.com>
2018-08-16 20:55:16 +0200

    Show indicator in FilterLink when fetching failed

--- src/components/FilterLink/FilterLink.js
+++ src/components/FilterLink/FilterLink.js
@@ -6,19 +6,20 @@ import { Loader } from '../Loader';
 
 import './FilterLink.scss';
 
-export const FilterLink = ({ filter, isSelected, onClick }) => (
-  <div
-    key={filter.id}
-    className={cx('FilterLink', isSelected && 'FilterLink--selected')}
-    onClick={onClick}
-  >
-    <span className="FilterLink__ItemsCount">
-      {filter.loading ? (
-        <Loader size={13} color="#abacb9" strokeWidth={12} />
-      ) : (
-        size(filter.data)
-      )}
-    </span>
-    <bdi>{filter.label}</bdi>
-  </div>
-);
+export const FilterLink = ({ filter, isSelected, onClick }) => {
+  const { loading, error } = filter;
+  return (
+    <div
+      key={filter.id}
+      className={cx('FilterLink', isSelected && 'FilterLink--selected')}
+      onClick={onClick}
+    >
+      <span className="FilterLink__ItemsCount">
+        {loading && <Loader size={13} color="#abacb9" strokeWidth={12} />}
+        {!loading && error && <i className="fa fa-times" />}
+        {!loading && !error && size(filter.data)}
+      </span>
+      <bdi>{filter.label}</bdi>
+    </div>
+  );
+};
