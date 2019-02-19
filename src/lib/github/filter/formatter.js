f90eecfa80c936d2999a3a34808c5923dc0ebb8d
Renan GEHAN <rgehan94@gmail.com>
2019-02-20 00:28:15 +0100

    Fetch issues using the REST api when no token is set

--- src/lib/github/filter/formatter.js
+++ src/lib/github/filter/formatter.js
@@ -1,22 +1,63 @@
-import { chain, get, omit } from 'lodash';
+import { chain, get, omit, map, pick } from 'lodash';
 
-export const formatFilter = filter =>
+/*
+ * GraphQL
+ */
+
+export const formatGraphqlResponse = filter =>
   chain(filter)
     .get('data.search.edges')
     .map('node')
     .map(issue => ({
       ...omit(issue, ['commits', '__typename']),
       type: issue.__typename,
-      status: extractStatus(issue),
-      labels: extractLabels(issue),
+      status: extractGraphqlStatus(issue),
+      labels: extractGraphqlLabels(issue),
     }))
     .value();
 
-const extractStatus = issue =>
+const extractGraphqlStatus = issue =>
   get(issue, 'commits.edges.0.node.commit.status.state');
 
-const extractLabels = issue =>
+const extractGraphqlLabels = issue =>
   chain(issue)
     .get('labels.edges')
     .map('node')
     .value();
+
+/*
+ * REST
+ */
+
+export const formatRestResponse = response => {
+  return map(response, issue => ({
+    url: issue.html_url,
+    title: issue.title,
+    number: issue.number,
+    state: issue.state,
+    createdAt: issue.created_at,
+    author: {
+      url: issue.user.html_url,
+      login: issue.user.login,
+      avatarUrl: issue.user.avatar_url,
+    },
+    repository: {
+      nameWithOwner: parseRestRepoName(issue.repository_url),
+      url: parseRestRepoUrl(issue.repository_url),
+    },
+    labels: extractRestLabels(issue),
+  }));
+};
+
+const extractRestLabels = issue =>
+  map(issue.labels, label => pick(label, ['color', 'name']));
+
+const parseRestRepoName = url =>
+  chain(url)
+    .split('/')
+    .slice(-2)
+    .join('/')
+    .value();
+
+const parseRestRepoUrl = apiUrl =>
+  apiUrl.replace('api.github.com/repos/', 'github.com/');
--- src/lib/github/filter/index.js
+++ src/lib/github/filter/index.js
@@ -1,11 +1,27 @@
 import { chain } from 'lodash';
 
 import { makeQuery } from './graphql';
-import { formatFilter } from './formatter';
+import { formatGraphqlResponse, formatRestResponse } from './formatter';
 import { githubClient } from '../client';
 import { serializePredicatePayload } from '../../filters';
 
+/**
+ * Fetch a filter using the proper strategy, depending on
+ * whether the user has set a token or not.
+ * @param {object} options.filter
+ * @param {string} options.token
+ */
 export const fetchFilter = async ({ filter, token }) => {
+  return token
+    ? fetchFilterWithGraphqlAPI({ filter, token })
+    : fetchFilterWithRestAPI({ filter });
+};
+
+/**
+ * Fetcha filter using the shiny GraphQL API
+ * @param {object} options.filter
+ */
+const fetchFilterWithGraphqlAPI = async ({ filter, token }) => {
   const filterString = chain(filter.predicates)
     .map(serializePredicatePayload)
     .join(' ')
@@ -18,5 +34,25 @@ export const fetchFilter = async ({ filter, token }) => {
     token,
   });
 
-  return formatFilter(response);
+  return formatGraphqlResponse(response);
+};
+
+/**
+ * Fetch a filter on the old REST API. This is only supposed to be
+ * used when the user has not set a token. This won't return all the
+ * value we need (for example there are no build status).
+ * @param {object} options.filter
+ */
+const fetchFilterWithRestAPI = async ({ filter }) => {
+  const filterString = chain(filter.predicates)
+    .map(serializePredicatePayload)
+    .join('+')
+    .value();
+
+  const { items: issues = [] } = await githubClient({
+    endpoint: '/search/issues',
+    qs: `per_page=100&q="${filterString}"`,
+  });
+
+  return formatRestResponse(issues);
 };
