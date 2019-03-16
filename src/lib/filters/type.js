ee1271d2512a7b05ba680a06a34d084c4601fe0e
Renan GEHAN <rgehan94@gmail.com>
2019-03-16 18:57:05 +0100

    Remove now useless filters lib

--- src/lib/filters/type.js
+++
@@ -1,9 +0,0 @@
-export const type = {
-  name: 'type',
-  label: 'Type',
-  type: 'dropdown',
-  choices: [{ value: 'pr', label: 'PRs' }, { value: 'issue', label: 'Issues' }],
-  negatable: false,
-  isAvailable: filters => !filters.has('type'),
-  serialize: ({ value, negated }) => `${negated ? '-' : ''}type:${value}`,
-};
