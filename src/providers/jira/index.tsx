7482eaa9487085f5734e033add3da09d7d377c76
Renan GEHAN <rgehan94@gmail.com>
2019-03-23 12:14:43 +0100

    Adapt jira provider to the settings changes

--- src/providers/jira/index.tsx
+++ src/providers/jira/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { find } from 'lodash';
 
 import { AbstractProvider } from '../AbstractProvider';
@@ -5,14 +6,25 @@ import { availablePredicates } from './predicates';
 import { Settings } from './components/Settings';
 import { IssueCard } from './components/IssueCard';
 import { fetchFilter } from './fetchers';
+import { Filter } from '../../store/filters';
 
-class JiraProvider extends AbstractProvider {
+export interface JiraSettings {
+  auth: {
+    access_token: string;
+    refresh_token: string;
+    scope: string;
+  };
+}
+
+class JiraProvider extends AbstractProvider<JiraSettings> {
   public id = 'jira';
   public label = 'Jira';
-  public settingsComponent = Settings;
+  public settingsComponent = () => <Settings settings={this.settings} />;
   public cardComponent = IssueCard;
 
-  public fetchFilter = fetchFilter;
+  public async fetchFilter(filter: Filter) {
+    return fetchFilter(filter, this.settings);
+  }
 
   public getAvailablePredicates = () => availablePredicates;
 
