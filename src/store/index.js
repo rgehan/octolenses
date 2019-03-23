fe82aba7e47108f47edb7ac17af8f79d00edeb8e
Renan GEHAN <rgehan94@gmail.com>
2019-03-23 12:15:15 +0100

    Ensure providers are persisted/hydrated just like regular stores

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,33 @@
 import { create } from 'mobx-persist';
+import { chain } from 'lodash';
 
 import { navigation } from './navigation';
 import { filters } from './filters';
 import { trends } from './trends';
 import { settings } from './settings';
-import { ProviderType } from '../providers';
+import { providers, ProviderType } from '../providers';
 
 import { migrateData } from './migrations';
 
 const hydrateStores = async () => {
   const hydrate = create({});
+
+  // Re-hydrate the stores
   await Promise.all([
     hydrate('navigationStore', navigation),
     hydrate('settingsStore', settings),
     hydrate('filtersStore', filters),
   ]);
+
+  // Re-hydrate the providers
+  await Promise.all(
+    chain(providers)
+      .values()
+      .map(provider => {
+        hydrate(`${provider.id}Provider`, provider);
+      })
+      .value()
+  );
 };
 
 const performOnboarding = async () => {
