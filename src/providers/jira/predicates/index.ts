24e4a8e34ff8c93de5c3480ae02ca940c89570f2
Renan GEHAN <rgehan94@gmail.com>
2019-03-23 11:11:56 +0100

    Simplify providers by extracting behavior to other files

--- src/providers/jira/predicates/index.ts
+++ src/providers/jira/predicates/index.ts
@@ -23,3 +23,11 @@ export const makeSimplePredicate = ({
   type: PredicateType.TEXT,
   serialize: ({ value, negated }) => `${name} ${negated ? '!=' : '='} ${value}`,
 });
+
+export const availablePredicates: Predicate[] = [
+  makeSimplePredicate({ name: 'project', placeholder: 'MYPROJECT' }),
+  makeSimplePredicate({ name: 'status', placeholder: 'open/closed' }),
+  makeSimplePredicate({ name: 'resolution', placeholder: '' }),
+  makeSimplePredicate({ name: 'sprint', placeholder: '' }),
+  makeSimplePredicate({ name: 'assignee', placeholder: '' }),
+];
