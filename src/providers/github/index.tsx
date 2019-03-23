9bfab1b13eb6918eba10f33179aa813ebf579f09
Renan GEHAN <rgehan94@gmail.com>
2019-03-23 14:00:52 +0100

    On init, fetch Github profile

--- src/providers/github/index.tsx
+++ src/providers/github/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { find } from 'lodash';
+import { action } from 'mobx';
 
 import { AbstractProvider } from '../AbstractProvider';
 import { availablePredicates } from './predicates';
@@ -7,9 +8,17 @@ import { fetchFilter } from './fetchers';
 import { Settings } from './components/Settings';
 import { IssueCard } from './components/IssueCard';
 import { Filter } from '../../store/filters';
+import { fetchProfile } from './fetchers/rest/profile';
 
 export interface GithubSettings {
   token: string;
+}
+
+export interface GithubProfile {
+  login: string;
+  name: string;
+  avatar_url: string;
+  html_url: string;
 }
 
 export class GithubProvider extends AbstractProvider<GithubSettings> {
@@ -17,6 +26,22 @@ export class GithubProvider extends AbstractProvider<GithubSettings> {
   public label = 'GitHub';
   public settingsComponent = () => <Settings provider={this} />;
   public cardComponent = IssueCard;
+
+  public profile: GithubProfile = null;
+
+  @action.bound
+  public async initialize() {
+    await this.fetchProfile();
+  }
+
+  @action.bound
+  public async fetchProfile() {
+    if (!this.settings.token) {
+      return;
+    }
+
+    this.profile = await fetchProfile(this.settings.token);
+  }
 
   public async fetchFilter(filter: Filter) {
     return fetchFilter(filter, this.settings);
