b4d0461924abdd5dc76e150addfc5c27b74d0989
Renan GEHAN <rgehan94@gmail.com>
2019-03-23 12:14:28 +0100

    Adapt github provider to the settings changes

--- src/providers/github/index.tsx
+++ src/providers/github/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { find } from 'lodash';
 
 import { AbstractProvider } from '../AbstractProvider';
@@ -5,14 +6,21 @@ import { availablePredicates } from './predicates';
 import { fetchFilter } from './fetchers';
 import { Settings } from './components/Settings';
 import { IssueCard } from './components/IssueCard';
+import { Filter } from '../../store/filters';
 
-class GithubProvider extends AbstractProvider {
+export interface GithubSettings {
+  token: string;
+}
+
+class GithubProvider extends AbstractProvider<GithubSettings> {
   public id = 'github';
   public label = 'GitHub';
-  public settingsComponent = Settings;
+  public settingsComponent = () => <Settings settings={this.settings} />;
   public cardComponent = IssueCard;
 
-  public fetchFilter = fetchFilter;
+  public async fetchFilter(filter: Filter) {
+    return fetchFilter(filter, this.settings);
+  }
 
   public getAvailablePredicates = () => availablePredicates;
 
