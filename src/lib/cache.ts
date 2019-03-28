54b528d639a50203ec693fbf26491af929493c1b
Renan GEHAN <rgehan94@gmail.com>
2019-03-28 02:07:18 +0100

    Implement a cache class (very similar to laravel Cache facade)

---
+++ src/lib/cache.ts
@@ -0,0 +1,71 @@
+interface CacheEntry<T> {
+  expiresAt: number;
+  value: T;
+}
+
+export class Cache {
+  /**
+   * Remember the result of an expensive computation or data fetching, during a
+   * certain period. The result is identified by a unique key.
+   * @param key Key at which to store the result of the computation
+   * @param lifespan How long, in second, to store the item
+   * @param computer The method that computes the value, if not in the cache
+   */
+  public static async remember<T = any>(
+    key: string,
+    lifespan: number,
+    computer: () => Promise<T>
+  ): Promise<T> {
+    const cached = Cache.get<T>(key);
+
+    if (cached) {
+      if (Date.now() < cached.expiresAt) {
+        return cached.value;
+      }
+
+      Cache.forget(key);
+    }
+
+    const value = await computer();
+    Cache.put(key, value, lifespan);
+    return value;
+  }
+
+  /**
+   * Retrieve an item from the cache
+   * @param key Key at which the item is stored
+   */
+  public static get<T = any>(key: string): CacheEntry<T> {
+    const item = localStorage.getItem(Cache.getPrefixedKey(key));
+
+    try {
+      return JSON.parse(item);
+    } catch (error) {}
+
+    return null;
+  }
+
+  /**
+   * Put an item into the cache
+   * @param key Key at which to store the cached item
+   * @param value Item to cache
+   * @param lifespan How long, in seconds, we want to keep the item
+   */
+  public static put(key: string, value: any, lifespan: number) {
+    localStorage.setItem(
+      Cache.getPrefixedKey(key),
+      JSON.stringify({
+        expiresAt: Date.now() + lifespan * 1000,
+        value,
+      })
+    );
+  }
+
+  public static forget(key: string) {
+    localStorage.removeItem(Cache.getPrefixedKey(key));
+  }
+
+  private static getPrefixedKey(key: string) {
+    return `cache:${key}`;
+  }
+}
