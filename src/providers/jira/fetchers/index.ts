b6cb30eee2f8053f7e0877c43de0d183be6b93f5
Renan GEHAN <rgehan94@gmail.com>
2019-03-29 11:55:53 +0100

    Cache jira filters too

--- src/providers/jira/fetchers/index.ts
+++ src/providers/jira/fetchers/index.ts
@@ -1,7 +1,9 @@
 import { chain, get } from 'lodash';
+import hash from 'object-hash';
 
 import { Filter } from '../../../store/filters';
 import { JiraSettings, JiraResource } from '..';
+import { Cache } from '../../../lib/cache';
 
 export async function fetchFilter(
   filter: Filter,
@@ -19,15 +21,17 @@ export async function fetchFilter(
 
   const url = `https://api.atlassian.com/ex/jira/${site}/rest/api/2/search?jql=${filterString}`;
 
-  const response = await fetch(url, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${token}`,
-    },
-  });
+  const cacheKey = `jira.filter.${hash(url)}`;
 
-  const { issues } = await response.json();
+  const { issues } = await Cache.remember(cacheKey, 60, () =>
+    fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    }).then(res => res.json())
+  );
 
   return issues;
 }
