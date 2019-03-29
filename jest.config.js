612ea27e556f429abcdad49f1edd01c74980849a
Renan GEHAN <rgehan94@gmail.com>
2019-03-29 16:09:35 +0100

    Disable ts diagnostics in tests

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,9 @@ module.exports = {
   preset: 'ts-jest/presets/js-with-ts',
   testEnvironment: 'node',
   setupFiles: ['./src/setupTests.ts'],
+  globals: {
+    'ts-jest': {
+      diagnostics: false,
+    },
+  },
 };
