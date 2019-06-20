d9da1fc97a1b7dbc528fcc0b4c68207d1e9f32ac
Renan Gehan <rgehan94@gmail.com>
2019-06-20 14:06:24 +0200

    Adapt CheckStatusIndicator to new status format

--- src/providers/github/components/IssueCard/CheckStatusIndicator.tsx
+++ src/providers/github/components/IssueCard/CheckStatusIndicator.tsx
@@ -1,19 +1,24 @@
 import cx from 'classnames';
 import React from 'react';
 
-import { Issue } from './IssueCard';
+import { Issue, IssueStatus } from './IssueCard';
 
 interface IProps {
   status: Issue['status'];
 }
 
+const STATUS_TO_ICON = {
+  [IssueStatus.SUCCESS]: 'fas fa-check text-green',
+  [IssueStatus.FAILURE]: 'fas fa-times text-red',
+  [IssueStatus.PENDING]: 'fas fa-circle text-orange',
+};
+
 export const CheckStatusIndicator = ({ status }: IProps) => {
-  if (!status) {
+  if (status === IssueStatus.UNKNOWN) {
     return null;
   }
 
-  const icon =
-    status === 'SUCCESS' ? 'fa-check text-green' : 'fa-times text-red';
+  const icon = STATUS_TO_ICON[status];
 
-  return <i className={cx('fa text-sm ml-2', icon)} />;
+  return <i className={cx('text-sm ml-2', icon)} />;
 };
