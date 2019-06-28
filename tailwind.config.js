b66093e80894326d93079ed907da1b620017e4b5
Renan GEHAN <rgehan94@gmail.com>
2019-06-28 23:47:04 +0200

    Update tailwindcss module to 1.0.4

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      open: ['Open Sans', 'sans-serif'],
+      roboto: ['Roboto', 'sans-serif'],
+      mono: [
+        'Menlo',
+        'Monaco',
+        'Consolas',
+        'Liberation Mono',
+        'Courier New',
+        'monospace',
+      ],
+    },
+    extend: {
+      fontSize: {
+        '2xs': '.625rem', // 10px
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
