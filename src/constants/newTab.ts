88ee6041b5fd14603a75ecd8dbee2069fdc8ef33
Renan GEHAN <rgehan94@gmail.com>
2023-07-05 20:47:10 +0200

    Fallback to localStorage if chrome.storage not available

--- src/constants/newTab.ts
+++ src/constants/newTab.ts
@@ -1,12 +1,21 @@
 const NEW_TAB_SETTING_LS_KEY = 'useNewTabPage';
 
 export async function getNewTabSetting() {
-  const {
-    [NEW_TAB_SETTING_LS_KEY]: results = true,
-  } = await chrome.storage.local.get([NEW_TAB_SETTING_LS_KEY]);
-  return results;
+  if (chrome.storage) {
+    const {
+      [NEW_TAB_SETTING_LS_KEY]: results = true,
+    } = await chrome.storage.local.get([NEW_TAB_SETTING_LS_KEY]);
+    return results;
+  } else {
+    const results = localStorage.getItem(NEW_TAB_SETTING_LS_KEY);
+    return results !== null ? JSON.parse(results) : true;
+  }
 }
 
 export function saveNewTabSetting(useNewTab: boolean) {
-  chrome.storage.local.get({ [NEW_TAB_SETTING_LS_KEY]: useNewTab });
+  if (chrome.storage) {
+    chrome.storage.local.get({ [NEW_TAB_SETTING_LS_KEY]: useNewTab });
+  } else {
+    localStorage.setItem(NEW_TAB_SETTING_LS_KEY, JSON.stringify(useNewTab));
+  }
 }
